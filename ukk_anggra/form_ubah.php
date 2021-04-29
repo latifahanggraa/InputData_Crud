6f978ab509b6d9b1fd5af7594cbb3b10193cb7e4
latifahanggraa <53440800+latifahanggraa@users.noreply.github.com>
2021-04-29 22:55:46 +0700

    Add files via upload

---
+++ ukk_anggra/form_ubah.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+  <title>Tugas UKK Anggra</title>
+</head>
+<body>
+  <h1>Ubah Data Siswa</h1>
+  <?php
+  // Load file koneksi.php
+  include "koneksi.php";
+  // Ambil data NIS yang dikirim oleh index.php melalui URL
+  $id = $_GET['id'];
+  // Query untuk menampilkan data siswa berdasarkan ID yang dikirim
+  $sql = $pdo->prepare("SELECT * FROM tb_ukk WHERE id=:id");
+  $sql->bindParam(':id', $id);
+  $sql->execute(); // Eksekusi query insert
+  $data = $sql->fetch(); // Ambil semua data dari hasil eksekusi $sql
+  ?>
+  <form method="post" action="proses_ubah.php?id=<?php echo $id; ?>" enctype="multipart/form-data">
+    <table cellpadding="8">
+      <tr>
+        <td>Nama</td>
+        <td><input type="text" name="nama" value="<?php echo $data['nama']; ?>"></td>
+      </tr>  
+      <tr>
+        <td>NIS</td>
+        <td><input type="text" name="nis" value="<?php echo $data['nis']; ?>"></td>
+      </tr>
+      <tr>
+        <td>Jenis Kelamin</td>
+        <td>
+        <?php
+        if($data['jenis_kelamin'] == "Laki-laki"){
+          echo "<input type='radio' name='jenis_kelamin' value='laki-laki' checked='checked'> Laki-laki";
+          echo "<input type='radio' name='jenis_kelamin' value='perempuan'> Perempuan";
+        }else{
+          echo "<input type='radio' name='jenis_kelamin' value='laki-laki'> Laki-laki";
+          echo "<input type='radio' name='jenis_kelamin' value='perempuan' checked='checked'> Perempuan";
+        }
+        ?>
+        </td>
+      </tr>
+      <tr>
+        <td>Jurusan</td>
+        <td><input type="text" name="jurusan" value="<?php echo $data['jurusan']; ?>"></td>
+      </tr>
+      <tr>
+        <td>Foto</td>
+        <td>
+          <input type="file" name="foto">
+        </td>
+      </tr>
+    </table>
+    <hr>
+    <input type="submit" value="Ubah">
+    <a href="index.php"><input type="button" value="Batal"></a>
+  </form>
+</body>
+</html>
